bfce4c008867702e81069fea4b8d55861493f752
Евгений Егоров <evstroyka@gmail.com>
2023-12-22 15:55:08 +0300

    commit

--- WPFclient/Models/FileData.cs
+++ WPFclient/Models/FileData.cs
@@ -7,6 +7,12 @@ namespace WPFclient.Models
         public string FileName { get; set; }
         public string FilePath { get; set; }
         public string LocalFileFolder { get; set; }
+        public string ChangeInfo { get; set; }
         public DateTime Date { get; set; }
+
+        public override string ToString()
+        {
+            return $"В плагине {FileName} добавлены следующие изменения {ChangeInfo}\n\n";
+        }
     }
 }
--- WPFclient/ViewModels/MainWindowVM.cs
+++ WPFclient/ViewModels/MainWindowVM.cs
@@ -190,6 +190,7 @@ namespace WPFclient.ViewModels
                     if (serverLastModified[i].Date > localLastModified[i])
                     {
                         await ApiManager.DownloadFileAsync(httpClient, ServerUrl, serverLastModified[i].FilePath, serverLastModified[i].LocalFileFolder);
+                        TextInfo += serverLastModified[i].ChangeInfo;
                     }
                 }
             }
