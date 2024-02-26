b8001a024bff0946c87eab5492223da88a28b5e6
Evgeny <evstroyka@gmail.com>
2024-02-26 23:34:12 +0300

    конвертеры

---
+++ WPFclient/Infrastructure/Converters/Converter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WPFclient.Infrastructure.Converters
+{
+    public abstract class Converter : IValueConverter
+    {
+        public abstract object Convert(object v, Type t, object p, CultureInfo c);
+
+        public virtual object ConvertBack(object v, Type t, object p, CultureInfo c) =>
+
+            throw new NotSupportedException("Обратное преобразование не поддерживается");
+    }
+}
