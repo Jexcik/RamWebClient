0ee2547758f9709fb0fdf8932721a045106e79d7
Евгений Егоров <evstroyka@gmail.com>
2024-02-27 17:10:59 +0300

    Запуск хоста при старте приложения и остановка при выходе

--- WPFclient/App.xaml.cs
+++ WPFclient/App.xaml.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Windows;
-using WPFclient.Services;
+using WPFclient.ViewModels;
 
 namespace WPFclient.Views
 {
@@ -14,6 +11,31 @@ namespace WPFclient.Views
     /// </summary>
     public partial class App : Application
     {
+        private static IHost _Host;
 
+        public static IHost Host = _Host ?? Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            var host = Host;
+            base.OnStartup(e);
+
+            await host.StartAsync().ConfigureAwait(false);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            var host = Host;
+            await host.StopAsync().ConfigureAwait(false);
+            host.Dispose();
+            _Host = null;
+        }
+
+        public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            services.AddSingleton<ViewModel>();
+        }
     }
 }
