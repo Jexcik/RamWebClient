cff930cfd78d992e1a556b70d40d9bb8206c9ee2
Евгений Егоров <evstroyka@gmail.com>
2024-02-27 16:32:37 +0300

    Собственная точка входа в приложение

--- WPFclient/Services/FileChangeService.cs
+++ WPFclient/Services/FileChangeService.cs
@@ -5,43 +5,8 @@ using WPFclient.Services.Interface;
 
 namespace WPFclient.Services
 {
-    public class FileChangeService : IFileChangeService
+    public class FileChangeService
     {
-        public event EventHandler<FileChangedEventArgs> FileChange;
 
-        private FileSystemWatcher fileSystemWatcher;
-
-        public void StartWatching(string dirPath)
-        {
-            fileSystemWatcher = new FileSystemWatcher
-            {
-                Path = dirPath,
-                Filter = ".txt",
-                NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName,
-                EnableRaisingEvents = true
-            };
-            fileSystemWatcher.Changed += FileSystemWatcher_Changed;
-        }
-
-        public void StopWatching()
-        {
-            fileSystemWatcher?.Dispose();
-        }
-
-        private void FileSystemWatcher_Changed(object sender, FileSystemEventArgs e)
-        {
-            OnFileChanged(new FileChangedEventArgs
-            {
-                FileName = e.Name,
-                FilePath=e.FullPath,
-                DateChange=DateTime.Now,
-                Author="ИМЯ_АВТОРА"
-            });
-        }
-
-        protected virtual void OnFileChanged(FileChangedEventArgs e)
-        {
-            FileChange?.Invoke(this, e);
-        }
     }
 }
