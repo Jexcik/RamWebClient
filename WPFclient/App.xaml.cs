d5668a9b6f715c61c999a2a40a3c1e065a3e9b77
Евгений Егоров <evstroyka@gmail.com>
2024-03-22 17:03:49 +0300

    commit

--- WPFclient/App.xaml.cs
+++ WPFclient/App.xaml.cs
@@ -6,9 +6,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows;
 using WPFclient.Services;
-using WPFclient.Services.Interfaces;
 using WPFclient.ViewModels;
-using WPFclient.ViewModels.TabItem;
 
 namespace WPFclient.Views
 {
